fe7667a98e9a8e80eb7add39d938932fe65e628f
rupalisarcar <rupalisarcar@gmail.com>
2025-11-07 01:13:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded",()=>{
+	const otpCode = document.querySelectorAll('.code');
+	otpCode.forEach((input,index)=>{
+		input.addEventListener('input',(e)=>{
+			if(e.target.value.length==1 && index<otpCode.length-1){
+				otpCode[index+1].focus()
+			}
+		})
+
+		input.addEventListener('keydown',(e)=>{
+			if(e.key==='Backspace' && e.target.value.length==0 && index>0){
+				otpCode[index-1].focus()
+			}
+		})
+	})
+})
