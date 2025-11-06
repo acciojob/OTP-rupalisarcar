3df969cc9c43b507d2eda2297a6dd78f6469a18b
rupalisarcar <rupalisarcar@gmail.com>
2025-11-07 01:27:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 //your JS code here. If required.
 document.addEventListener("DOMContentLoaded",()=>{
 	const otpCode = document.querySelectorAll('.code');
+	const firstOtp = document.getElementById('code-1');
+	firstOtp.focus();
 	otpCode.forEach((input,index)=>{
 		input.addEventListener('input',(e)=>{
 			if(e.target.value.length==1 && index<otpCode.length-1){
-				otpCode[index+1].focus()
+				otpCode[index+1].focus();
 			}
 		})
 
 		input.addEventListener('keydown',(e)=>{
 			if(e.key==='Backspace' && e.target.value.length==0 && index>0){
-				otpCode[index-1].focus()
+				otpCode[index-1].focus();
 			}
 		})
 	})
